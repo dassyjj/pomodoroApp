f08e2a9f3e04eb31278d4f6efbe2ab08a8ff575b
Jonathan Carvalho <developerjjonathan@gmail.com>
2023-07-21 21:49:01 -0300

    feat: Routes with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,15 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
 
-import { Fragment } from 'react'
-import { Button } from './components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Fragment>
-        <h1>Home</h1>
-
-        <Button variant='primary' />
-        <Button variant='secondary' />
-        <Button variant='danger' />
-        <Button variant='success' />
-      </Fragment>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyles />
     </ThemeProvider>
