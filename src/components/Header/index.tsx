2ee60789f791fc930149055ce540e641ad2cfcdc
Jonathan Carvalho <developerjjonathan@gmail.com>
2023-07-22 18:47:45 -0300

    feat: Header & Layout Styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
+import { HeaderContainer } from "./styles";
+
+import Pomodoro from '../../assets/Logo.svg'
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+
 export function Header() {
-  return <header><h1>header</h1></header>
+  return (
+    <HeaderContainer>
+      <NavLink to="/"><img src={Pomodoro} alt="" /></NavLink>
+
+      <nav>
+        <NavLink to="/" title="Timer"><Timer size={24} /></NavLink>
+        <NavLink to="/history" title="History"><Scroll size={24} /></NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
