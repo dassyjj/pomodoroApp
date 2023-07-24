46bf3c6fe28882739feed00583422ee3aae874e7
Jonathan Carvalho <developerjjonathan@gmail.com>
2023-07-23 23:06:50 -0300

    feat: History Component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,34 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History() {
   return (
-    <h1>History</h1>
+    <HistoryContainer>
+      <h1>My historic</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Init</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="yellow">
+                  Success
+                </Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
